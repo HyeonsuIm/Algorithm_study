8bdf611e8916ff6ee1f25f2d22857ec1ed687746
hyeonsu.Im <yis1308@gmail.com>
2022-09-04 22:22:25 +0900

    solve

--- bkjn11054_longest_bitonic.py
+++ bkjn11054_longest_bitonic.py
@@ -1,20 +1,25 @@
 from sys import stdin
 
-num_cnt = int(stdin.readline().rstrip())
-numbers = list(map(int, stdin.readline().split()))
+data_cnt = int(stdin.readline().rstrip())
+datas = list(map(int, stdin.readline().split()))
+dp_front = [0 for _ in range(data_cnt)]
+dp_rear = [0 for _ in range(data_cnt)]
+for i in range(data_cnt):
+    BEST_INCREASE_CNT = 0
+    for j in range(i):
+        if datas[j] < datas[i]:
+            BEST_INCREASE_CNT = max(BEST_INCREASE_CNT, dp_front[j])
+    dp_front[i] = BEST_INCREASE_CNT + 1
 
-result = [[[0,0,0] for _ in range(num_cnt+1)] for _ in range(num_cnt+1)]
-for i in range(num_cnt):
-    for j in range(num_cnt):
-        if result[i-1][j-1][2] == 0:
-            numbers[i]
-        else:
+for i in reversed(range(data_cnt)):
+    BEST_DECREASE_CNT = 0
+    for j in range(i+1, data_cnt):
+        if datas[j] < datas[i]:
+            BEST_DECREASE_CNT = max(BEST_DECREASE_CNT, dp_rear[j])
+    dp_rear[i] = BEST_DECREASE_CNT + 1
 
+MAX_BITONIC_SUBSEQUENCE = 0
+for i in range(data_cnt):
+    MAX_BITONIC_SUBSEQUENCE = max(dp_front[i]+dp_rear[i]-1, MAX_BITONIC_SUBSEQUENCE)
 
-
-for x in result:
-    if MAX_CNT < x:
-        MAX_CNT = x
-
-print(x)
-        
+print(MAX_BITONIC_SUBSEQUENCE)
