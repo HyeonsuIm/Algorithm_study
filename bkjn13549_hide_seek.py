578c14ac985175935324c192d886b694fd84cea2
hyeonsu.Im <yis1308@gmail.com>
2022-09-06 21:23:21 +0900

    solve

--- bkjn13549_hide_seek.py
+++ bkjn13549_hide_seek.py
@@ -1,56 +1,31 @@
 from sys import stdin
-from math import log2
+from heapq import heappush, heappop
+from math import inf
 
-def to_binary(start):
-    log_val = int(log2(start))
-    binary = []
-    for i in reversed(range(log_val+1)):
-        binary.append((start >> i) & 1)
+N, K = map(int,stdin.readline().split())
 
-    return binary
-
-
-def to_int(start):
-    val = 0
-    for i in start_bin:
-        val*=2
-        val+=i
-    return val
-
-
-def make_same_length(start_bin, target_bin):
-    start_len = len(start_bin)
-    target_len = len(target_bin)
-    move_cnt=0
-    current_move_cnt = 0
-    for i in range(start_len):
-        current_move_cnt *=2
-        if start_bin[i] != target_bin[i]:
-            if target_bin[i] == 1:
-                current_move_cnt+=1
-            else:
-                current_move_cnt-=1
-            start_bin[i] =target_bin[i]
-    move_cnt += abs(current_move_cnt)
-    while True:
-        if start_len == target_len:
+if K <= N:
+    print(N-K)
+else:
+    expand_info = [inf for _ in range(200001)]
+    heap = []
+    heappush(heap, [0, N])
+    RESULT = 0
+    while heap:
+        cost, position = heappop(heap)
+        if position == K:
+            RESULT = cost
             break
+        
+        if expand_info[position] <= cost:
+            continue
 
-        start_bin.append(target_bin[start_len])
-        if target_bin[start_len] == 1:
-            move_cnt+=1
-        start_len+=1
-    return move_cnt
-
-
-
-start, target = map(int, stdin.readline().split())
-start_bin = to_binary(start)
-target_bin = to_binary(target)
-
-move_cnt = 0
-if start < target:
-    move_cnt = make_same_length(start_bin, target_bin)
-    start = to_int(start_bin)
-    target = to_int(target)
-print(move_cnt + abs(start-target))
+        expand_info[position] = cost
+        if position < K:
+            heappush(heap, [cost+1, position+1])
+            heappush(heap, [cost+1, position*2])
+        
+        if position != 0:
+            heappush(heap, [cost+1, position-1])
+    
+    print(RESULT)
