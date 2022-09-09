b8431db1c5c60ce554b560f72f0ecbc298e6aca4
hyeonsu.Im <yis1308@gmail.com>
2022-09-09 11:59:29 +0900

    solve

---
+++ bkjn2448_drawing_star.py
@@ -0,0 +1,24 @@
+from sys import stdin
+
+def divide_cases(draw_map, base_x, base_y, size):
+    if size != 3:
+        next_size = size//2
+        divide_cases(draw_map, base_x, base_y, next_size)
+        divide_cases(draw_map, base_x-next_size, base_y+next_size, next_size)
+        divide_cases(draw_map, base_x+next_size, base_y+next_size, next_size)
+    else:
+        draw_map[base_y][base_x]='*'
+        draw_map[base_y+1][base_x-1]=draw_map[base_y+1][base_x+1]='*'
+        for k in range(-2,3):
+            draw_map[base_y+2][base_x-k]='*'
+
+
+
+line_cnt = int(stdin.readline().rstrip())
+draw_area = [[' ' for _ in range(line_cnt*2)] for _ in range(line_cnt)]
+
+divide_cases(draw_area, line_cnt-1, 0, line_cnt)
+
+result = ''
+for i in range(line_cnt):
+    print(*draw_area[i], sep='')
