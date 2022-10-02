fef5f25a86725bc3f10f5436bd85b5527ffabd01
hyeonsu.Im <yis1308@gmail.com>
2022-10-02 09:46:19 +0900

    solve

---
+++ cpp/bkjn9086_string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    for(int i=0;i<T;i++)
+    {
+        string str;
+        cin >> str;
+        cout << str[0] << str[str.length()-1] << endl;
+    }
+}
