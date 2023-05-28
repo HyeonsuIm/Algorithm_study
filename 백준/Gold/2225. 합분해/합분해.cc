02b7eb1314fd34c1d11b8bdfe946ef6c5d0536ad
HyeonsuIm <32991938+HyeonsuIm@users.noreply.github.com>
2023-05-28 12:38:17 +0900

    [Gold V] Title: 합분해, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.cc
+++ 백준/Gold/2225. 합분해/합분해.cc
@@ -1,21 +1,17 @@
 #include <iostream>
-
 using namespace std;
+
 int dp[201][201];
+
 int main()
 {
     int N, K;
     cin >> N >> K;
 
-    for(int i=1;i<=K;i++) dp[i][0] = 1;
-
+    for(int i=1;i<=K;i++) dp[0][i] = 1;
     for(int i=1;i<=K;i++)
     {
-        for(int j=1;j<=N;j++)
-        {
-            dp[i][j] = (dp[i][j-1]+dp[i-1][j])%1000000000;
-        }
+        for(int j=1;j<=N;j++) dp[j][i] = (dp[j][i-1]+dp[j-1][i]) % 1000000000;
     }
-
-    cout << dp[K][N];
+    cout << dp[N][K];
 }
