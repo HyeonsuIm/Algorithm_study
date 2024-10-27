a0ebf23d36d53b4bfdd73805982fe77a45acb272
HyeonsuIm <32991938+HyeonsuIm@users.noreply.github.com>
2024-10-27 11:22:07 +0900

    [Silver II] Title: 제곱수의 합, Time: 216 ms, Memory: 111300 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -1,18 +1,11 @@
 from sys import stdin
 import math
 
-def is_square(n):
-    sqrt_n = int(math.sqrt(n))
-    return sqrt_n * sqrt_n == n
-
 val = int(stdin.readline())
 dp = [0 for _ in range(val+1)]
 for i in range(1,val+1):
     dp[i] = i
-    if is_square(i):
-        dp[i] = 1
-    else:
-        if int(math.sqrt(i)) > 1:
-            dp[i] = min(dp[i], min([dp[i-j*j]+1 for j in range(1,int(math.sqrt(i)))]))
+    if int(math.sqrt(i)) > 1:
+        dp[i] = min(dp[i], min([dp[i-j*j]+1 for j in range(1,int(math.sqrt(i)+1))]))
     
 print(dp[val])
