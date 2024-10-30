76b08c6dc8f5c0319eafe301e320e8cbc62afa34
HyeonsuIm <32991938+HyeonsuIm@users.noreply.github.com>
2024-10-30 21:33:22 +0900

    [Gold IV] Title: 합이 0, Time: 4732 ms, Memory: 111432 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.py
+++ 백준/Gold/3151. 합이 0/합이 0.py
@@ -19,17 +19,12 @@ for start in range(0, N-2):
     end = N-1
     while mid < end:
         sum = values[start]+values[mid]+values[end]
-        if sum == 0:
-            if values[mid] == values[end]:
-                count_sum += end - mid
-            else:
+        if sum > 0:
+            end-=1
+        else:
+            if sum == 0:
                 left = find_left(mid+1, end, values[end])
                 count_sum += end - left + 1
             mid +=1
-        elif sum > 0:
-            end-=1
-        else:
-            mid +=1
-        
     
 print(count_sum)
